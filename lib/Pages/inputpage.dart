643d43eb8abcc03b8ed2eea669ddd5a8aed9bbf1
AustinSeries <austinseriez@gmail.com>
2022-09-07 16:19:32 +0530

    input page

---
+++ lib/Pages/inputpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
