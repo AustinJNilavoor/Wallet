cd177549ad9ab204e6214da561adfb3140b74a3b
AustinSeries <austinseriez@gmail.com>
2022-09-08 12:39:55 +0530

    commit before adding DB

--- lib/Pages/inputpage.dart
+++ lib/Pages/inputpage.dart
@@ -13,6 +13,10 @@ class _InputPageState extends State<InputPage> {
   bool isExpense = true;
   bool isTicked = false;
   bool isExpanded = false;
+  bool isMultiple = false;
+  bool isTransfer = false;
+  String dropdownmvalue = 'Amazon Pay';
+  String dropdowntvalue = 'Wallet';
 
   String dropdown1value = 'Others';
 
@@ -200,11 +204,16 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
             Padding(
-              padding: const EdgeInsets.only(right: 8.0,left: 8),
+              padding: const EdgeInsets.only(right: 8.0, left: 8),
               child: InkWell(
                 borderRadius: BorderRadius.circular(17),
                 onTap: () {
                   setState(() {
+                    if (isMultiple || isTransfer) {
+                      isMultiple = false;
+                      isTransfer = false;
+                    }
+
                     isExpanded = !isExpanded;
                   });
                 },
@@ -324,18 +333,19 @@ class _InputPageState extends State<InputPage> {
               borderRadius: BorderRadius.circular(17),
               onTap: () {
                 setState(() {
-                  isTicked = !isTicked;
+                  if (isTransfer) isTransfer = false;
+                  isMultiple = !isMultiple;
                 });
               },
               child: SizedBox(
                 height: 50,
-                width: 150,
+                // width: 150,
                 child: Row(
                   children: [
                     const SizedBox(
                       width: 5,
                     ),
-                    isTicked
+                    isMultiple
                         ? Icon(
                             Icons.check_box,
                             color: Colors.blue,
@@ -347,7 +357,7 @@ class _InputPageState extends State<InputPage> {
                     const SizedBox(
                       width: 10,
                     ),
-                    const Text('Add to Tab',
+                    const Text('Payment from multiple Accounts',
                         style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.bold,
@@ -358,7 +368,155 @@ class _InputPageState extends State<InputPage> {
             ),
           ],
         ),
+        isMultiple ? buildMultiOpt() : SizedBox(),
+        Row(
+          children: [
+            const SizedBox(
+              width: 8,
+            ),
+            InkWell(
+              borderRadius: BorderRadius.circular(17),
+              onTap: () {
+                setState(() {
+                  if (isMultiple) isMultiple = false;
+                  isTransfer = !isTransfer;
+                });
+              },
+              child: SizedBox(
+                height: 50,
+                // width: 150,
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    isTransfer
+                        ? Icon(
+                            Icons.check_box,
+                            color: Colors.blue,
+                          )
+                        : Icon(
+                            Icons.check_box_outline_blank,
+                            color: Colors.white70,
+                          ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text('Add Transfer',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white70)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        isTransfer
+            ? Ink(
+                height: 50,
+                width: (MediaQuery.of(context).size.width - 24),
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade900,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 14),
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 20,
+                    ),
+                    elevation: 0,
+                    dropdownColor: Colors.grey.shade900,
+                    value: dropdowntvalue,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: items2.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdowntvalue = newValue!;
+                      });
+                    },
+                  ),
+                ),
+              )
+            : SizedBox(),
+        const Divider(
+          indent: 12,
+          endIndent: 12,
+          color: Colors.grey,
+          thickness: 1.2,
+        ),
       ],
+    );
+  }
+
+  Widget buildMultiOpt() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            height: 50,
+            width: (MediaQuery.of(context).size.width - 24) / 2,
+            child: TextFormField(
+              decoration: new InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: new BorderRadius.circular(20.0)),
+                  filled: true,
+                  fillColor: Colors.grey.shade900,
+                  hintText: "Amount",
+                  prefixIcon: Icon(Icons.money, color: Colors.white70),
+                  contentPadding: EdgeInsets.all(10),
+                  hintStyle: TextStyle(color: Colors.white70)),
+              style: TextStyle(color: Colors.white70),
+              cursorColor: Colors.white70,
+            ),
+          ),
+          Ink(
+            height: 50,
+            width: (MediaQuery.of(context).size.width - 24) / 2,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade900,
+                borderRadius: BorderRadius.circular(20)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14),
+              child: DropdownButton(
+                isExpanded: true,
+                underline: SizedBox(),
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 20,
+                ),
+                elevation: 0,
+                dropdownColor: Colors.grey.shade900,
+                value: dropdownmvalue,
+                icon: const Icon(Icons.keyboard_arrow_down),
+                items: items2.map((String items) {
+                  return DropdownMenuItem(
+                    value: items,
+                    child: Text(items),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownmvalue = newValue!;
+                  });
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
